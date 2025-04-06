14489bab6543afe0ced5f242b017350fe2092e1d
ychebotaev <yury.79120345101@gmail.com>
2025-04-06 19:57:39 +0500

    fix: add debug logging

--- lib/bots/BotsService.ts
+++ lib/bots/BotsService.ts
@@ -51,6 +51,8 @@ export class BotsService {
     this.dockhost = new DockhostService(dockhostToken)
     this.logger = logger
 
+    console.log('this.db:', this.db)
+
     if (siteOrigin != null) {
       this.siteOrigin = siteOrigin
     }
