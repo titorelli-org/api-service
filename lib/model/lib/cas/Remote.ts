1670e2da2a0cb95160ff029c3ec760a745a6b709
ychebotaev <yury.79120345101@gmail.com>
2025-04-08 22:17:08 +0500

    feat: introduce remote cas filter

---
+++ lib/model/lib/cas/Remote.ts
@@ -0,0 +1,45 @@
+import type { Logger } from "pino";
+import type { ICas } from "./types";
+
+export type IsBannedResult = {
+  banned: boolean;
+};
+
+export class RemoteAntispam implements ICas {
+  private baseUrl = process.env.CAS_ORIGIN;
+
+  constructor(private logger: Logger) {}
+
+  async has(tgUserId: number): Promise<boolean> {
+    try {
+      const url = new URL("/isBanned", this.baseUrl);
+
+      url.searchParams.set("tgUserId", String(tgUserId));
+
+      const resp = await fetch(url);
+      const { banned } = (await resp.json()) as Awaited<IsBannedResult>;
+
+      return banned;
+    } catch (error) {
+      this.logger.error(error);
+
+      return false;
+    }
+  }
+
+  async add(tgUserId: number): Promise<void> {
+    try {
+      const url = new URL("/train", this.baseUrl);
+
+      await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({ tgUserId, banned: true }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      this.logger.error(error);
+    }
+  }
+}
